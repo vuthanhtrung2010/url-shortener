312216e7c511f98609babe2ed4aecbdef64f7b1f
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-07-16 14:44:42 +0000

    into env

--- instrumentation.server.mjs
+++ instrumentation.server.mjs
@@ -1,7 +1,8 @@
 import * as Sentry from "@sentry/remix";
+import "dotenv/config";
 
 Sentry.init({
-    dsn: "https://e6e13b4e8ed78464ea377a9ea9e58d59@o4507611654717440.ingest.de.sentry.io/4507611659370576",
+    dsn: process.env.SENTRY_DSN_ADDRESS,
     tracesSampleRate: 1,
     autoInstrumentRemix: true
 })

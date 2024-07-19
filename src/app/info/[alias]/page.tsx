c4bbcb9a6a6bb60e742acaa6418866abf4f8f698
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-07-19 14:41:15 +0000

    fixes

--- src/app/info/[alias]/page.tsx
+++ src/app/info/[alias]/page.tsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 import { getData } from '../../../data';
 
 interface Props {
-    params: { alias: string };
+    params: Readonly<{ alias: string }>;
 }
 
 export default async function InfoPage({ params }: Props) {
@@ -20,15 +20,17 @@ export default async function InfoPage({ params }: Props) {
 
     console.log(chalk.green(`Found ${link} with alias ${alias}!`));
 
+    const href = `/${link.aliases[0]}`
+
     return (
-        <body className="p-8 m-auto bg-zinc-900 text-white">
+        <div className="p-8 m-auto bg-zinc-900 text-white">
             <header className="mb-4">
                 <h1 className="text-2xl font-bold">Redirect Information</h1>
             </header>
-            <p><span className="font-semibold">Path</span>: <a className="text-blue-500 hover:text-blue-600" href="/<%= data.path %>"></a>Path: /{link.aliases[0]}</p>
+            <p><span className="font-semibold">Path</span>: <a className="text-blue-500 hover:text-blue-600" href={href}></a> /{link.aliases[0]}</p>
             <p><span className="font-semibold">Redirect</span>: <a className="text-blue-500 hover:text-blue-600" href={link.link}>{link.link}</a></p>
             <p><span className="font-semibold">Created</span>: {moment(link.createdAt).format('Do MMMM YYYY, h:mma')}</p>
             <p><span className="font-semibold">Hits</span>: {link.hits}</p>
-        </body>
+        </div>
     );
 }

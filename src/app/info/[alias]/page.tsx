1b7c93da946e1afa61ba7b2c94f5b7e826d439bb
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-07-19 14:46:19 +0000

    better layout

--- src/app/info/[alias]/page.tsx
+++ src/app/info/[alias]/page.tsx
@@ -7,7 +7,7 @@ interface Props {
     params: Readonly<{ alias: string }>;
 }
 
-export default async function InfoPage({ params }: Props) {
+export default async function InfoPage({ params }: Readonly<Props>) {
     const { alias } = params;
 
     console.log(chalk.blue(`Requesting data for alias: ${alias}`));
@@ -20,17 +20,27 @@ export default async function InfoPage({ params }: Props) {
 
     console.log(chalk.green(`Found ${link} with alias ${alias}!`));
 
-    const href = `/${link.aliases[0]}`
+    const href = `/${link.aliases[0]}`;
 
     return (
         <div className="p-8 m-auto bg-zinc-900 text-white">
             <header className="mb-4">
                 <h1 className="text-2xl font-bold">Redirect Information</h1>
             </header>
-            <p><span className="font-semibold">Path</span>: <a className="text-blue-500 hover:text-blue-600" href={href}></a> /{link.aliases[0]}</p>
-            <p><span className="font-semibold">Redirect</span>: <a className="text-blue-500 hover:text-blue-600" href={link.link}>{link.link}</a></p>
-            <p><span className="font-semibold">Created</span>: {moment(link.createdAt).format('Do MMMM YYYY, h:mma')}</p>
-            <p><span className="font-semibold">Hits</span>: {link.hits}</p>
+            <p>
+                <span className="font-semibold">Path</span>: 
+                <a className="text-blue-500 hover:text-blue-600" href={href}> /{link.aliases[0]}</a>
+            </p>
+            <p>
+                <span className="font-semibold">Redirect</span>: 
+                <a className="text-blue-500 hover:text-blue-600" href={link.link}> {link.link}</a>
+            </p>
+            <p>
+                <span className="font-semibold">Created</span>: {moment(link.createdAt).format('Do MMMM YYYY, h:mma')}
+            </p>
+            <p>
+                <span className="font-semibold">Hits</span>: {link.hits}
+            </p>
         </div>
     );
 }

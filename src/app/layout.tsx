19aade93a5555c0e717a2140c5b9d8d9e5328cf2
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-07-21 09:08:55 +0000

    add credit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <div className="credit" id="credit">
+          Made by <a href="https://discord.gg/TR8k3MtjNZ">Vũ Thành Trung</a> |{" "}
+          <a href="https://github.com/vuthanhtrung2010/spotify-status">
+            Github
+          </a>
+        </div>
+
+        <div className="space-y-0">
+          <p className="mt-8 text-base leading-8 text-center text-gray-400">
+            &copy; 2024 Trung - All Rights Reserved.
+          </p>
+        </div>
+      </body>
     </html>
   );
 }

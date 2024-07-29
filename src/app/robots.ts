b6b0c7d39912c48377c17d7ea320d1ed41110f5b
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-07-29 11:28:20 +0000

    site mapping & robots

---
+++ src/app/robots.ts
@@ -0,0 +1,18 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: [
+            {
+                userAgent: "Googlebot",
+                allow: "/",
+                disallow: "/dashboard"
+            },
+            {
+                userAgent: "Applebot",
+                disallow: ['/', "/dashboard"],
+            },
+        ],
+        sitemap: 'https://links.trung.is-a.dev/sitemap.xml',
+    }
+}
---
+++ src/app/sitemap.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: "https://links.trung.is-a.dev",
+            lastModified: new Date(),
+            changeFrequency: "yearly"
+        },
+        {
+            url: "https://links.trung.is-a.dev/dashboard",
+            lastModified: new Date(),
+            changeFrequency: "yearly"
+        }
+    ]
+}

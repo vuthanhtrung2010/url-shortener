dd0345f13433d1c0ace0188fe9843bd826075655
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-08-23 07:50:13 +0000

    refactor: Update LoadingToast component to include loading message

--- src/components/Toasts/loading.tsx
+++ src/components/Toasts/loading.tsx
@@ -1,12 +1,14 @@
 export function LoadingToast() {
-    <div className="max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-neutral-800 dark:border-neutral-700" role="alert" tabIndex={-1} aria-labelledby="hs-toast-message-with-loading-indicator-label">
-        <div className="flex items-center p-4">
-            <div className="animate-spin inline-block size-4 border-[3px] border-current border-t-transparent text-blue-600 rounded-full" role="status" aria-label="loading">
-                <span className="sr-only">Loading...</span>
+    return (
+        <div className="max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-neutral-800 dark:border-neutral-700" role="alert" tabIndex={-1} aria-labelledby="hs-toast-message-with-loading-indicator-label">
+            <div className="flex items-center p-4">
+                <div className="animate-spin inline-block size-4 border-[3px] border-current border-t-transparent text-blue-600 rounded-full" role="status" aria-label="loading">
+                    <span className="sr-only">Loading...</span>
+                </div>
+                <p id="hs-toast-message-with-loading-indicator-label" className="ms-3 text-sm text-gray-700 dark:text-neutral-400">
+                    Action in progress
+                </p>
             </div>
-            <p id="hs-toast-message-with-loading-indicator-label" className="ms-3 text-sm text-gray-700 dark:text-neutral-400">
-                Action in progress
-            </p>
         </div>
-    </div>
+    )
 }

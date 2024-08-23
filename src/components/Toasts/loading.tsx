bf92e015bd6ccac682dd247b1ef7a84cbf5cf68e
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-08-23 07:48:49 +0000

    refactor: Remove unnecessary CSS classes from LoadingToast component

--- src/components/Toasts/loading.tsx
+++ src/components/Toasts/loading.tsx
@@ -1,5 +1,5 @@
 export function LoadingToast() {
-    <div className="hs-removing:translate-x-5 hs-removing:opacity-0 transition duration-300 max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-neutral-800 dark:border-neutral-700" role="alert" tabIndex={-1} aria-labelledby="hs-toast-message-with-loading-indicator-label">
+    <div className="max-w-xs bg-white border border-gray-200 rounded-xl shadow-lg dark:bg-neutral-800 dark:border-neutral-700" role="alert" tabIndex={-1} aria-labelledby="hs-toast-message-with-loading-indicator-label">
         <div className="flex items-center p-4">
             <div className="animate-spin inline-block size-4 border-[3px] border-current border-t-transparent text-blue-600 rounded-full" role="status" aria-label="loading">
                 <span className="sr-only">Loading...</span>

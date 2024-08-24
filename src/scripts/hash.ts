977e1d967644f8ded77f75bd028d56375bbf22b8
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-08-24 12:32:40 +0000

    chore: Using SHA3-512 instead SHA3-256

--- src/scripts/hash.ts
+++ src/scripts/hash.ts
@@ -1,7 +1,7 @@
 import { createHash } from 'crypto';
 
 export function hash(input: string): string {
-    const hash = createHash('sha3-256');
+    const hash = createHash('sha3-512');
     hash.update(input);
     return hash.digest('hex');
 }

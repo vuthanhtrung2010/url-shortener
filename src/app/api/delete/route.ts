66e2cc5bc1efe3cf558a71b4f1be82afa9964424
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-08-26 04:46:49 +0000

    chore: Update password comparison to use base64 decoding

--- src/app/api/delete/route.ts
+++ src/app/api/delete/route.ts
@@ -17,7 +17,7 @@ export async function POST(request: Request) {
     );
   }
 
-  const checkPassword = compareSync(password, process.env.PASSWORD_HASH);
+  const checkPassword = compareSync(password, Buffer.from(process.env.PASSWORD_HASH, "base64").toString("utf-8"));
   if (!checkPassword) {
     return NextResponse.json(
       { success: false, message: "Invalid password." },

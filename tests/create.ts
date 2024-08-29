719fa6f3a327050bb8cca1659bb254f7dee11f2a
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-08-29 02:27:25 +0000

    chore: Remove createRedirect test

--- tests/create.ts
+++
@@ -1,7 +0,0 @@
-import { prismaMock } from '../singleton';
-import { createRedirect } from '../src/data';
-
-test('createRedirect function exists', () => {
-    console.log(createRedirect); // Should log the function, not undefined
-    expect(typeof createRedirect).toBe('function');
-  });

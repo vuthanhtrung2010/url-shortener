a71e2d74d3c150a3959b5b71c5b1c04903a8980d
Vũ Thành Trung <vuthanhtrungsuper@gmail.com>
2024-12-14 11:35:13 +0000

    delete source map after update.

--- next.config.ts
+++ next.config.ts
@@ -51,8 +51,9 @@ export default MillionLint.next({
     // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
     // side errors will fail.
     tunnelRoute: "/monitoring",
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
+    sourcemaps: {
+      deleteSourcemapsAfterUpload: true,
+    },
     // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
     // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
